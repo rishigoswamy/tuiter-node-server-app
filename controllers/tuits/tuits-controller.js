46dd514e341bac9fb75dcbb0e50b4d40167cdf9c
Rishi Goswamy <rishigoswamy@Rishis-MacBook-Pro.local>
2022-12-01 21:32:54 -0500

    Schema changes

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,11 +1,58 @@
-import * as tuitsDao from '../tuits/tuits-dao.js'
+import * as tuitsDao from './tuits-dao.js'
 
+const currentUser = {
+
+};
+
+const templateTuit = {
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "replies": 0,
+    "retuits": 0,
+    "likes": 0,
+    "dislikes": 0,
+    "comments": 0
+}
+
+const createTuit = async(req, res) => {
+    const newTuit = req.body;
+    newTuit._id = parseInt((new Date()).getTime()+'');
+    newTuit.userName = "NASA";
+    newTuit.handleName = "nasa";
+    newTuit.image = "nasa.jpg";
+    newTuit.topic = "Space";
+    newTuit.time = "2h";
+    newTuit.liked = false;
+    newTuit.disliked = false;
+    newTuit.replies = 0;
+    newTuit.retuits = 0;
+    newTuit.likes = 0;
+    newTuit.dislikes = 0;
+    newTuit.comments = 0
+    const insertedTuit = await tuitsDao
+        .createTuit(newTuit);
+    res.json(insertedTuit);
+}
 const findTuits = async (req, res) => {
     const tuits = await tuitsDao.findTuits()
     res.json(tuits);
 }
-
-
+const updateTuit = async(req, res) => {
+    const tuitdIdToUpdate = parseInt(req.params.tid);
+    const updates = req.body;
+    const status = await tuitsDao
+        .updateTuit(tuitdIdToUpdate,
+            updates);
+    res.json(status);
+}
+const deleteTuit = async (req, res) => {
+    const tuitdIdToDelete = parseInt(req.params.tid);
+    const status = await tuitsDao
+        .deleteTuit(tuitdIdToDelete);
+    res.json(status);
+}
 
 export default (app) => {
     app.post('/api/tuits', createTuit);
@@ -13,34 +60,3 @@ export default (app) => {
     app.put('/api/tuits/:tid', updateTuit);
     app.delete('/api/tuits/:tid', deleteTuit);
 }
-
-const createTuit = async (req, res) => {
-    const newTuit = req.body;
-
-    newTuit.likes = 0;
-    newTuit.liked = false;
-    
-    const insertedTuit = await tuitsDao.createTuit(newTuit);
-    res.json(insertedTuit);
-}
-
-
-
-const deleteTuit = async (req, res) => {
-    const tuitdIdToDelete = req.params.tid;
-    const status = await tuitsDao
-        .deleteTuit(tuitdIdToDelete);
-
-    res.json(status);
-}
-
-const updateTuit = async (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
-    const updates = req.body;
-    const status = await tuitsDao
-        .updateTuit(tuitdIdToUpdate,
-            updates);
-    res.json(status);
-}
-
-
--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,8 +1,17 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
+    _id:Number,
     tuit: String,
     likes: Number,
     liked: Boolean,
+    topic: String,
+    userName: String,
+    title: String,
+    time: String,
+    image: String,
+    replies: Number,
+    retuits: Number,
+    handle: String,
 }, {collection: 'tuits'});
 export default schema;
 
